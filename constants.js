102d654583a03c5ecf565102939b640dc953eaa1
malingahasantha <malinga.hasantha@gmail.com>
2024-08-06 21:49:44 +0530

    hello, variable and constant

---
+++ constants.js
@@ -0,0 +1,13 @@
+
+let radius = 5
+const pi = 3.14
+let area
+
+radius = 7
+area = pi * radius * radius
+
+console.log(area)
+
+/*value of pi is a constant. no one can't change it. we can't change it arter declare.
+but radius can change after declare
+*/
---
+++ variables.js
@@ -0,0 +1,13 @@
+let num = 2+2
+//let is a keyword to declare a variable.
+
+//once you declare a variable, again you don't need to declare it again and again.
+//only special characters allowed are number, $ and _
+num = 9
+
+let user = 'Malinga'
+
+console.log(num)
+console.log(user)
+
+console.log(num + 3)
